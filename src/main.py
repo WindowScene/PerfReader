c7f9b28ac91b28dc91ce84f82ca295334b0e9dc4
Kirill <kk90@mail.com>
2022-01-08 12:46:21 -0800

    testing dockerfile

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
+import sys
+
 
 def print_log_name(log_file_name):
     print(f'Log file name: {log_file_name}')
 
 
 if __name__ == '__main__':
-    print_log_name('Some log file.log ')
+    print_log_name(sys.argv[1])
